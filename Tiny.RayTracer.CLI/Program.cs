6da85fa801aa5778b886140ffdb06877c863d7a8
Logan Payton <Logan.Payton@javs.com>
2019-01-26 16:33:29 -0500

    :sparkles: Support multiple spheres

--- Tiny.RayTracer.CLI/Program.cs
+++ Tiny.RayTracer.CLI/Program.cs
@@ -14,11 +14,23 @@ namespace Tiny.RayTracer.CLI
             const string filePath = "./out.ppm";
 
             var frameBuffer = new FrameBuffer(width, height);
+
+            var ivory = new Material(new Vector3(0.4f, 0.4f, 0.3f), Vector3.Zero, 0.0f);
+            var rubber = new Material(new Vector3(0.3f, 0.1f, 0.1f), Vector3.Zero, 0.0f);
+
+            var spheres = new[]
+            {
+                new Sphere(new Vector3(-3.0f, 0.0f, -16.0f), 2.0f, ivory),
+                new Sphere(new Vector3(-1.0f, -1.5f, -12.0f), 2.0f, rubber),
+                new Sphere(new Vector3(1.5f, -0.5f, -18.0f), 3.0f, rubber),
+                new Sphere(new Vector3(7.0f, 5.0f, -18.0f), 4.0f, ivory),
+            };
+
             var renderer = new FlatRenderer
             {
                 FieldOfView = MathF.PI / 2.0f,
-                Sphere = new Sphere(new Vector3(-3.0f, 0.0f, -16.0f), 2.0f, new Material(new Vector3(0.2f, 0.7f, 0.8f), Vector3.Zero, 0.0f)),
-                BackgroundColor = new Vector3(0.4f, 0.4f, 0.3f)
+                Spheres = spheres,
+                BackgroundColor = new Vector3(0.2f, 0.7f, 0.8f)
             };
 
             renderer.Render(frameBuffer);
--- Tiny.RayTracer.Core/Rendering/FlatRenderer.cs
+++ Tiny.RayTracer.Core/Rendering/FlatRenderer.cs
@@ -1,17 +1,41 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace Tiny.RayTracer.Core.Rendering
 {
     public class FlatRenderer : RayTracingRenderer
     {
-        public Sphere Sphere { get; set; }
+        public IList<Sphere> Spheres { get; set; }
         public Vector3 BackgroundColor { get; set; }
 
         protected override Vector3 Cast(Ray ray)
         {
-            return Sphere.Intersects(ray, out _)
-                ? Sphere.Material.DiffuseColor
+            return SceneIntersect(ray, out _, out var material)
+                ? material.DiffuseColor
                 : BackgroundColor;
+        }
+
+        private bool SceneIntersect(Ray ray, out Ray hit, out Material material)
+        {
+            hit = null;
+            material = null;
+
+            float intersectionDistance = float.MaxValue;
+            foreach (var sphere in Spheres)
+            {
+                if (sphere.Intersects(ray, out float distance) && distance < intersectionDistance)
+                {
+                    intersectionDistance = distance;
+                    var position = ray.Origin + ray.Direction * distance;
+                    var direction = Vector3.Normalize(position - sphere.Center);
+
+                    hit = new Ray(position, direction);
+                    material = sphere.Material;
+                }
+            }
+
+            // TODO: Replace magic number
+            return intersectionDistance < 1000.0f;
         }
     }
 }
