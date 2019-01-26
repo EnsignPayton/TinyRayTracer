8e2bead29c15024d76af1ff3491536574e599ef5
Logan Payton <Logan.Payton@javs.com>
2019-01-26 10:10:11 -0500

    :sparkles: Add base projects

---
+++ Tiny.RayTracer.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tiny.RayTracer.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
