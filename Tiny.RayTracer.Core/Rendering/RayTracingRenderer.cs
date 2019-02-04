0955fc70787422b387f3079493802bc248c9912f
Logan Payton <Logan.Payton@javs.com>
2019-02-04 00:37:07 -0500

    Use TPL and Stopwatch to time it - about a 5x speedup

--- Tiny.RayTracer.Core/Rendering/RayTracingRenderer.cs
+++ Tiny.RayTracer.Core/Rendering/RayTracingRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Numerics;
+using System.Threading.Tasks;
 
 namespace Tiny.RayTracer.Core.Rendering
 {
@@ -10,30 +11,35 @@ namespace Tiny.RayTracer.Core.Rendering
 
         public override void Render(FrameBuffer frameBuffer)
         {
-            for (int j = 0; j < frameBuffer.Height; j++)
+            Parallel.For(0, frameBuffer.Height, j =>
             {
                 for (int i = 0; i < frameBuffer.Width; i++)
                 {
-                    // Calculate ray direction from pixel position
-                    var halfScreenWidth = MathF.Tan(FieldOfView / 2.0f);
-                    var x = (2.0f * (i + 0.5f) / frameBuffer.Width - 1.0f) *
-                            halfScreenWidth *
-                            frameBuffer.Width / frameBuffer.Height;
-
-                    var y = -(2.0f * (j + 0.5f) / frameBuffer.Height - 1.0f) *
-                            halfScreenWidth;
-
-                    // Image is projected onto the z = -1 plane
-                    var direction = Vector3.Normalize(new Vector3(x, y, -1.0f));
-
-                    // TODO: Factor in rotation
-                    var ray = new Ray(Camera.Origin, direction);
-
-                    var color = Cast(ray);
-
-                    frameBuffer.Fill(i, j, color);
+                    RenderPixel(frameBuffer, i, j);
                 }
-            }
+            });
+        }
+
+        private void RenderPixel(FrameBuffer frameBuffer, int i, int j)
+        {
+            // Calculate ray direction from pixel position
+            var halfScreenWidth = MathF.Tan(FieldOfView / 2.0f);
+            var x = (2.0f * (i + 0.5f) / frameBuffer.Width - 1.0f) *
+                    halfScreenWidth *
+                    frameBuffer.Width / frameBuffer.Height;
+
+            var y = -(2.0f * (j + 0.5f) / frameBuffer.Height - 1.0f) *
+                    halfScreenWidth;
+
+            // Image is projected onto the z = -1 plane
+            var direction = Vector3.Normalize(new Vector3(x, y, -1.0f));
+
+            // TODO: Factor in rotation
+            var ray = new Ray(Camera.Origin, direction);
+
+            var color = Cast(ray);
+
+            frameBuffer.Fill(i, j, color);
         }
 
         protected abstract Vector3 Cast(Ray ray);
